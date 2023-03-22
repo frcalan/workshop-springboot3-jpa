3d73b036ac30f3c9aca6c432ffa5bfea24921960
frcalan <frcalan@gmail.com>
2023-03-22 20:11:11 -0300

    Service layer, component registration

---
+++ src/main/java/io/productdemo/demo/seervices/UserService.java
@@ -0,0 +1,27 @@
+package io.productdemo.demo.seervices;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.productdemo.demo.entities.User;
+import io.productdemo.demo.repositores.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
